81bc763f526651f7dd300ec911eda7d60eb6443a
priyadebnath20 <134611100+priyadebnath20@users.noreply.github.com>
2023-05-25 17:35:42 +0530

    Add files via upload

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+
+        public void PrintFulName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        public float YearlySalary;
+    }
+
+    public class PartTimeEmployee : Employee
+    {
+        public float HourlySalary;
+
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FullTimeEmployee FTE = new FullTimeEmployee();
+            FTE.FirstName = "Piku";
+            FTE.LastName = "Singh";
+            FTE.YearlySalary = 5000000;
+            FTE.PrintFulName();
+        }
+    }
+}
