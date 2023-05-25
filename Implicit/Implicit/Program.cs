da7685ed2788765d92b18fec70bfe2001b6cf2f3
priyadebnath20 <134611100+priyadebnath20@users.noreply.github.com>
2023-05-25 17:34:02 +0530

    Add files via upload

---
+++ Implicit/Implicit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Implicit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1223;
+
+            float f = i;
+
+            Console.WriteLine(f);
+        }
+    }
+}
