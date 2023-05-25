d0efc272daedef5e797b1a537cb5051c7aa20066
priyadebnath20 <134611100+priyadebnath20@users.noreply.github.com>
2023-05-25 17:37:02 +0530

    Add files via upload

---
+++ Operator1/Operator1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Operator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = 10;
+            int AnotherNumber = 21;
+
+            if(Number ==10 || AnotherNumber == 20)
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    public class Employee
+    {
+        public string FirstName = "Shreya";
+        public string LastName = "Barua";
+
+
+        public void PrintFullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+
+    public class PartTimeEmployee : Employee
+    {
+
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+
+    }
+
+    public class TemporaryEmployee : Employee
+    {
+
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[4];
+
+            employees[0] = new Employee();
+            employees[1] = new PartTimeEmployee();
+            employees[2] = new FullTimeEmployee();
+            employees[3] = new TemporaryEmployee();
+
+            foreach(Employee e in employees)
+            {
+                e.PrintFullName();
+            }
+           
+
+
+
+        }
+    }
+}
