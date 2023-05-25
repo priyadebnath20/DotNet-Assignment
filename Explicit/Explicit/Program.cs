f63ce597dfa7bd1c26a607800a1280065acacd11
priyadebnath20 <134611100+priyadebnath20@users.noreply.github.com>
2023-05-25 17:32:51 +0530

    Add files via upload

---
+++ Explicit/Explicit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Explicit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float f = 564.55F;
+
+            int i = Convert.ToInt32(f);
+
+            Console.WriteLine(i);
+
+    }
+}
